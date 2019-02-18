6c4ec72c84f955fe4cfd4f7fb6484483f9d4a3a4
danimaster91 <danimaster91@outlook.de>
2019-02-18 20:33:06 +0100

    da

--- Receive_Transmit/src/receive_transmit.h
+++ Receive_Transmit/src/receive_transmit.h
@@ -3,4 +3,22 @@
 #include<nRF24L01.h>
 #include<RF24.h>
 #include<SPI.h>
+#include <string>
+
+class receive_transmit{
+public:
+  explicit receive_transmit(const std::string& n)
+  : name_(n), punktzahl_(0){}
+
+const std::string& name() const{
+  return name_;
+}
+int punktzahl_() const {
+  return punktzahl_;
+}
+private:
+  std::string name_;
+  int punktzahl_;
+};
+
 #endif
