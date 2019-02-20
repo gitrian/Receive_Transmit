c1f3670e4bad6f0b5f23eb83a0671e4570effc94
danimaster91 <danimaster91@outlook.de>
2019-02-20 21:51:57 +0100

    class with radio

--- Receive_Transmit/src/receive_transmit.cpp
+++ Receive_Transmit/src/receive_transmit.cpp
@@ -1,5 +1,7 @@
 #include "Arduino.h"
 #include "receive_transmit.h"
+#include<nRF24L01.h>
+#include<RF24.h>
 
 Receive_Transmit::Receive_Transmit(int pin1, int pin2, bool receive)
 {
@@ -7,14 +9,15 @@ Receive_Transmit::Receive_Transmit(int pin1, int pin2, bool receive)
   _pin1 = pin1;
   _pin2 = pin2;
   _receive = receive;
+
 }
 
 void Receive_Transmit::initialize()
 {
-const byte address[6]="00001";  
+const byte address[6]="00001";
 if(_receive==true){
   //initialization for receiving data
-  /*radio.begin();
+  radio.begin();
   radio.openReadingPipe(0,address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
@@ -24,12 +27,12 @@ else{
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();*/
+  radio.stopListening();
 }}
 
 void Receive_Transmit::transmit(int x)
 {
-  //radio.write(&x, sizeof(x));
+  radio.write(&x, sizeof(x));
 }
 
 
--- Receive_Transmit/src/receive_transmit.h
+++ Receive_Transmit/src/receive_transmit.h
@@ -6,16 +6,16 @@
 
 
 class Receive_Transmit{
-public:
-  RF24 radio(int pin1,int pin2);
-  Receive_Transmit(int pin1,int pin2, bool receive);
-  void initialize();
-  void dash();
-  void transmit(int x);
 private:
   int _pin1;
   int _pin2;
   bool _receive;
+public:
+  RF24 radio = RF24(_pin1,_pin2);
+  Receive_Transmit(int pin1,int pin2, bool receive);
+  void initialize();
+  void dash();
+  void transmit(int x);
 };
 
 #endif
